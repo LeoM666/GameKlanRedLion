3fac44fa4268154483e2dc51160852458c2ccf7b
Leo <leonboiko061@gmail.com>
2019-07-02 06:17:16 +0300

    Initial Commit

--- MainHall/views.py
+++ MainHall/views.py
@@ -5,6 +5,3 @@ def mainhall(request):
     my_data = AboutMainHall.objects.all()
     contaxt = {"prTitle": "Главный зал", "data": my_data}
     return render(request, "connects/main_parts.html", contaxt)
-    
-
-
