bd3a66e69d1b7ad7da472144508287a3a97eb706
Leo <leonboiko061@gmail.com>
2019-07-09 11:30:40 +0300

    Initial Commit

---
+++ wrapper/static/wrapper/js/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.menuActive').hide()
+});
+
+$('.menu').click(function () {
+   $('.menuActive').show()
+});
+
+$('.menuOut').click(function () {
+    $('.menuActive').hide()
+});
